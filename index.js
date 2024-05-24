2172c3b9083aee08c867348053a526accce59574
p.karari <kararikelvin2002@gmail.com>
2024-05-24 18:43:50 +0300

    update

--- index.js
+++ index.js
@@ -32,31 +32,32 @@ function createCountryElement(country) {
   }
 
   function displayCountries(countries) {
+    countryCard.innerHTML = '';
     countries.forEach(country => {
       const countryElement = createCountryElement(country);
       countryCard.appendChild(countryElement);
     });
   }
 
-  function updateCountryCards() {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            const updateCountryCards = document.querySelectorAll('#countryCard .country');
-            updateCountryCards.forEach((card, index) => {
-                if (index < data.length) {
-                    const country = data[index];
-                    card.querySelector('img').src = country.flags.png;
-                    card.querySelector('.countryName').textContent = country.name;
-                    card.querySelector('.population').innerHTML = `<b>Population: </b>${country.population.toLocaleString()}`;
-                    card.querySelector('.region').innerHTML = `<b>Region: </b>${country.region}`;
+  // function updateCountryCards() {
+  //   fetch('data.json')
+  //       .then(response => response.json())
+  //       .then(data => {
+  //           const updateCountryCards = document.querySelectorAll('#countryCard .country');
+  //           updateCountryCards.forEach((card, index) => {
+  //               if (index < data.length) {
+  //                   const country = data[index];
+  //                   card.querySelector('img').src = country.flags.png;
+  //                   card.querySelector('.countryName').textContent = country.name;
+  //                   card.querySelector('.population').innerHTML = `<b>Population: </b>${country.population.toLocaleString()}`;
+  //                   card.querySelector('.region').innerHTML = `<b>Region: </b>${country.region}`;
                   
-                }
+  //               }
               
-            });
-        })
-        .catch(error => console.error('Error fetching the JSON file:', error));
-  }
+  //           });
+  //       })
+  //       .catch(error => console.error('Error fetching the JSON file:', error));
+  // }
 
   fetch('data.json')
     .then(response => response.json())
@@ -66,3 +67,56 @@ function createCountryElement(country) {
 
     })
     .catch(error => console.error('Error fetching the JSON file:', error));
+
+function filterDataByKeyword(data, keyword, property) {
+  return data.filter(item => item[property].toLowerCase().includes(keyword.toLowerCase()));
+}
+
+function filterDataByRegion(data, region) {
+  return data.filter(item => item.region.toLowerCase() === region.toLowerCase());
+}
+
+fetch('data.json')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(jsonData => {
+      
+      const filtered = document.getElementById("filterMenu")
+      const keyInput = document.getElementById("searchCountry")
+      const btn = document.getElementById("btn")
+
+      btn.addEventListener('click', () => {
+        const selectedRegion = filtered.value;
+        const keyword = keyInput.value.trim()
+        let filteredData = filterDataByRegion(data, selectedRegion)
+
+        if(keyword) {
+          filteredData = filterDataByKeyword(filteredData, keyword, 'name');
+        }
+      })
+
+      filtered.addEventListener('change', (event) => {
+        const selectedRegion = event.target.value;
+        const all = "All"
+        if (selectedRegion === all) {
+          displayCountries(jsonData)
+        }
+        else {
+          const filteredData = filterDataByRegion(jsonData, selectedRegion);
+        
+        displayCountries(filteredData)
+        }
+        
+      })
+      
+  })
+  .catch(error => {
+      console.error('There was a problem with your fetch operation:', error);
+  });
+
+//switch from dark to light theme
+
