a7a1d465e8410e10ac0b4f7af74e85cacd82801b
p.karari <kararikelvin2002@gmail.com>
2024-05-24 09:45:41 +0300

    Updates logic of the app to include the multiple countries

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ function updateCountryCards() {
         .then(data => {
             // console.log(data)
             const updateCountryCards = document.querySelectorAll('#countryCards1 .country')
-            const updateCountryCards2 = document.querySelectorAll('#countryCards2 .country')
-
             updateCountryCards.forEach((card, index) => {
                 if (index < data.length) {
                     const country = data[index];
@@ -24,40 +22,4 @@ function updateCountryCards() {
 }
 
 updateCountryCards()
-updateCountryCards2()
 
-// function createCountryCard(imageUrl, countryName, population, region) {
-//     const card = document.createElement('div');
-//     card.classList.add('country');
-
-//     const img = document.createElement('img');
-//     img.classList.add('img');
-//     img.src = imageUrl;
-//     img.alt = '';
-
-//     const name = document.createElement('p');
-//     name.classList.add('countryName');
-//     name.textContent = countryName;
-
-//     const pop = document.createElement('p');
-//     pop.classList.add('population');
-//     pop.innerHTML = `<b>Population: </b>${population}`;
-
-//     const reg = document.createElement('p');
-//     reg.classList.add('region');
-//     reg.innerHTML = `<b>Region: </b>${region}`;
-
-//     card.appendChild(img);
-//     card.appendChild(name);
-//     card.appendChild(pop);
-//     card.appendChild(reg);
-
-//     return card;
-// }
-
-// // Populate container with country cards
-// imageUrls.forEach((url, index) => {
-//     const card = createCountryCard(url, 'Germany', '81,770,900', 'Capital');
-//     container.appendChild(card);
-// });
-// // createCountryCard()
