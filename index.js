f7755cee7b477c1e25acbc05e35ca57349e95858
Kelvin Karari <125536010+p-karari@users.noreply.github.com>
2024-05-25 10:07:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,26 +39,6 @@ function createCountryElement(country) {
     });
   }
 
-  // function updateCountryCards() {
-  //   fetch('data.json')
-  //       .then(response => response.json())
-  //       .then(data => {
-  //           const updateCountryCards = document.querySelectorAll('#countryCard .country');
-  //           updateCountryCards.forEach((card, index) => {
-  //               if (index < data.length) {
-  //                   const country = data[index];
-  //                   card.querySelector('img').src = country.flags.png;
-  //                   card.querySelector('.countryName').textContent = country.name;
-  //                   card.querySelector('.population').innerHTML = `<b>Population: </b>${country.population.toLocaleString()}`;
-  //                   card.querySelector('.region').innerHTML = `<b>Region: </b>${country.region}`;
-                  
-  //               }
-              
-  //           });
-  //       })
-  //       .catch(error => console.error('Error fetching the JSON file:', error));
-  // }
-
   fetch('data.json')
     .then(response => response.json())
     .then(data => {
